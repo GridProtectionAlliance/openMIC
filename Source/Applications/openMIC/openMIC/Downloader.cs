165605c84431367fe0a2e6415040216371e6a0ca
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2016-11-03 10:42:13 -0400

    Fixed local file name to ensure path combination and path slash de-duplication still works with UNC paths

--- Source/Applications/openMIC/openMIC/Downloader.cs
+++ Source/Applications/openMIC/openMIC/Downloader.cs
@@ -1267,7 +1267,8 @@ namespace openMIC
 
             directoryNameExpressionParser.TemplatedExpression = settings.DirectoryNamingExpression.Replace("\\", "\\\\");
 
-            fileName = $"{settings.LocalPath}{Path.DirectorySeparatorChar}{directoryNameExpressionParser.Execute(substitutions)}{Path.DirectorySeparatorChar}{localSubPath}{Path.DirectorySeparatorChar}{fileName}".RemoveDuplicates(Path.DirectorySeparatorChar.ToString());
+            //         Possible UNC Path                            Sub Directory - duplicate path slashes are removed
+            fileName = FilePath.AddPathSuffix(settings.LocalPath) + "{directoryNameExpressionParser.Execute(substitutions)}{Path.DirectorySeparatorChar}{localSubPath}{Path.DirectorySeparatorChar}{fileName}".RemoveDuplicates(Path.DirectorySeparatorChar.ToString());
 
             string directoryName = FilePath.GetDirectoryName(fileName);
 
