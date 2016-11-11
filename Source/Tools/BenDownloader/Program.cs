30df341d80df701294e75a1686e6229dfe0d9e6a
Billy Ernest <bernest@gridprotectionalliance.org>
2016-11-11 10:40:58 -0500

    Added Ben Downloader to openMic built in tools

---
+++ Source/Tools/BenDownloader/Program.cs
@@ -0,0 +1,83 @@
+﻿//******************************************************************************************************
+//  Program.cs - Gbtc
+//
+//  Copyright © 2016, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
+//  not use this file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  10/06/2016 - Billy Ernest
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using GSF;
+using GSF.Configuration;
+using GSF.Data;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using Microsoft.VisualBasic.FileIO;
+using FileSystem = Microsoft.VisualBasic.FileIO.FileSystem;
+
+namespace OpenMICBenDownloadApp
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            if(args.Length != 2)
+            {
+                Console.WriteLine("Please pass two and only two parameters...");
+                return;
+            }
+
+            //ConfigurationFile openMicConfigurationFile = ConfigurationFile.Open("");
+            BenRunner br = new BenRunner(int.Parse(args[0]), int.Parse(args[1]));
+            if (!br.XferAllFiles()) 
+                throw new Exception("BEN Downloader failed...");
+
+        }
+
+        #region [Helper Functions]
+        public static string Left(string str, int length)
+        {
+            str = (str ?? string.Empty);
+            return str.Substring(0, Math.Min(length, str.Length));
+        }
+
+        public static string Right(string str, int length)
+        {
+            str = (str ?? string.Empty);
+            return (str.Length >= length)
+                ? str.Substring(str.Length - length, length)
+                : str;
+        }
+        #endregion
+
+    }
+}
