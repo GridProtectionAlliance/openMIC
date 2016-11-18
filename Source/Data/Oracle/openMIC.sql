3ae69d6007857c82a8c87c26b0f028712a73c0d6
gsfbuildbot <support@gridprotectionalliance.org>
2016-11-18 00:20:36 -0500

    Updated GSF dependencies.

--- Source/Data/Oracle/openMIC.sql
+++ Source/Data/Oracle/openMIC.sql
@@ -2704,4 +2704,48 @@ CREATE TRIGGER AI_ConnectionProfileTask BEFORE INSERT ON ConnectionProfileTask
 END;
 /
 
+ALTER TABLE ConnectionProfileTask ADD CONSTRAINT FK_ConnectionProfileTask_ConnectionProfile FOREIGN KEY(ConnectionProfileID) REFERENCES ConnectionProfile (ID); 
+CREATE TABLE ConnectionProfile(
+    ID NUMBER NOT NULL,
+    Name VARCHAR2(200) NOT NULL,
+    Description VARCHAR2(4000) NULL,
+    CreatedOn DATE NOT NULL,
+    CreatedBy VARCHAR2(200) NOT NULL,
+    UpdatedOn DATE NOT NULL,
+    UpdatedBy VARCHAR2(200) NOT NULL
+);
+
+CREATE UNIQUE INDEX IX_ConnectionProfile_ID ON ConnectionProfile (ID ASC) TABLESPACE openMIC_INDEX;
+
+ALTER TABLE ConnectionProfile ADD CONSTRAINT PK_ConnectionProfile PRIMARY KEY (ID);
+
+CREATE SEQUENCE SEQ_ConnectionProfile START WITH 1 INCREMENT BY 1;
+
+CREATE TRIGGER AI_ConnectionProfile BEFORE INSERT ON ConnectionProfile
+    FOR EACH ROW BEGIN SELECT SEQ_ConnectionProfile.nextval INTO :NEW.ID FROM dual;
+END;
+/
+
+CREATE TABLE ConnectionProfileTask(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    ConnectionProfileID NUMBER DEFAULT 1 NOT NULL,
+    Name VARCHAR2(200) NOT NULL,
+    Settings VARCHAR2(4000) NULL,
+    CreatedOn DATE NOT NULL,
+    CreatedBy VARCHAR2(200) NOT NULL,
+    UpdatedOn DATE NOT NULL,
+    UpdatedBy VARCHAR2(200) NOT NULL
+);
+
+CREATE UNIQUE INDEX IX_ConnectionProfileTask_ID ON ConnectionProfileTask (ID ASC) TABLESPACE openMIC_INDEX;
+
+ALTER TABLE ConnectionProfileTask ADD CONSTRAINT PK_ConnectionProfileTask PRIMARY KEY (ID);
+
+CREATE SEQUENCE SEQ_ConnectionProfileTask START WITH 1 INCREMENT BY 1;
+
+CREATE TRIGGER AI_ConnectionProfileTask BEFORE INSERT ON ConnectionProfileTask
+    FOR EACH ROW BEGIN SELECT SEQ_ConnectionProfileTask.nextval INTO :NEW.ID FROM dual;
+END;
+/
+
 ALTER TABLE ConnectionProfileTask ADD CONSTRAINT FK_ConnectionProfileTask_ConnectionProfile FOREIGN KEY(ConnectionProfileID) REFERENCES ConnectionProfile (ID);
