64080a062385d81d5976db8dca529ceaa10465aa
gsfbuildbot <support@gridprotectionalliance.org>
2016-11-27 00:06:11 -0500

    Updated GSF dependencies.

--- Source/Data/SQL Server/openMIC.sql
+++ Source/Data/SQL Server/openMIC.sql
@@ -3460,4 +3460,47 @@ GO
 
 ALTER TABLE [dbo].[ConnectionProfileTask]  WITH CHECK ADD  CONSTRAINT [FK_ConnectionProfileTask_ConnectionProfile] FOREIGN KEY([ConnectionProfileID])
 REFERENCES [dbo].[ConnectionProfile] ([ID])
+GO 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ConnectionProfile](
+    [ID] [int] IDENTITY(1,1) NOT NULL,
+    [Name] [varchar](200) NOT NULL,
+    [Description] [varchar](max) NULL,
+    [CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_ConnectionProfile_CreatedOn]  DEFAULT (getutcdate()),
+    [CreatedBy] [varchar](200) NOT NULL CONSTRAINT [DF_ConnectionProfile_CreatedBy]  DEFAULT (suser_name()),
+    [UpdatedOn] [datetime] NOT NULL CONSTRAINT [DF_ConnectionProfile_UpdatedOn]  DEFAULT (getutcdate()),
+    [UpdatedBy] [varchar](200) NOT NULL CONSTRAINT [DF_ConnectionProfile_UpdatedBy]  DEFAULT (suser_name()),
+ CONSTRAINT [PK_ConnectionProfile] PRIMARY KEY CLUSTERED 
+(
+    [ID] ASC
+)WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ConnectionProfileTask](
+    [ID] [int] IDENTITY(1,1) NOT NULL,
+    [ConnectionProfileID] [int] NOT NULL CONSTRAINT [DF_ConnectionProfileTask_ConnectionProfileID] DEFAULT (0),
+    [Name] [varchar](200) NOT NULL,
+    [Settings] [varchar](max) NULL,
+    [CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_ConnectionProfileTask_CreatedOn]  DEFAULT (getutcdate()),
+    [CreatedBy] [varchar](200) NOT NULL CONSTRAINT [DF_ConnectionProfileTask_CreatedBy]  DEFAULT (suser_name()),
+    [UpdatedOn] [datetime] NOT NULL CONSTRAINT [DF_ConnectionProfileTask_UpdatedOn]  DEFAULT (getutcdate()),
+    [UpdatedBy] [varchar](200) NOT NULL CONSTRAINT [DF_ConnectionProfileTask_UpdatedBy]  DEFAULT (suser_name()),
+ CONSTRAINT [PK_ConnectionProfileTask] PRIMARY KEY CLUSTERED 
+(
+    [ID] ASC
+)WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[ConnectionProfileTask]  WITH CHECK ADD  CONSTRAINT [FK_ConnectionProfileTask_ConnectionProfile] FOREIGN KEY([ConnectionProfileID])
+REFERENCES [dbo].[ConnectionProfile] ([ID])
 GO
--- Source/Data/SQLite/openMIC.sql
+++ Source/Data/SQLite/openMIC.sql
@@ -2056,3 +2056,39 @@ BEGIN
     UPDATE ConnectionProfileTask SET CreatedOn = strftime('%Y-%m-%d %H:%M:%f') WHERE ROWID = NEW.ROWID AND CreatedOn = '';
     UPDATE ConnectionProfileTask SET UpdatedOn = strftime('%Y-%m-%d %H:%M:%f') WHERE ROWID = NEW.ROWID AND UpdatedOn = '';
 END;
+ 
+CREATE TABLE ConnectionProfile(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    Name VARCHAR(200) NOT NULL,
+    Description TEXT NULL,
+    CreatedOn DATETIME NOT NULL DEFAULT '',
+    CreatedBy VARCHAR(200) NOT NULL DEFAULT '',
+    UpdatedOn DATETIME NOT NULL DEFAULT '',
+    UpdatedBy VARCHAR(200) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE ConnectionProfileTask(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    ConnectionProfileID INTEGER NOT NULL DEFAULT 1,
+    Name VARCHAR(200) NOT NULL,
+    Settings TEXT NULL,
+    CreatedOn DATETIME NOT NULL DEFAULT '',
+    CreatedBy VARCHAR(200) NOT NULL DEFAULT '',
+    UpdatedOn DATETIME NOT NULL DEFAULT '',
+    UpdatedBy VARCHAR(200) NOT NULL DEFAULT '',
+    CONSTRAINT FK_ConnectionProfileTask_ConnectionProfile FOREIGN KEY(ConnectionProfileID) REFERENCES ConnectionProfile (ID)
+);
+
+CREATE TRIGGER ConnectionProfile_InsertDefault AFTER INSERT ON ConnectionProfile
+FOR EACH ROW
+BEGIN
+    UPDATE ConnectionProfile SET CreatedOn = strftime('%Y-%m-%d %H:%M:%f') WHERE ROWID = NEW.ROWID AND CreatedOn = '';
+    UPDATE ConnectionProfile SET UpdatedOn = strftime('%Y-%m-%d %H:%M:%f') WHERE ROWID = NEW.ROWID AND UpdatedOn = '';
+END;
+
+CREATE TRIGGER ConnectionProfileTask_InsertDefault AFTER INSERT ON ConnectionProfileTask
+FOR EACH ROW
+BEGIN
+    UPDATE ConnectionProfileTask SET CreatedOn = strftime('%Y-%m-%d %H:%M:%f') WHERE ROWID = NEW.ROWID AND CreatedOn = '';
+    UPDATE ConnectionProfileTask SET UpdatedOn = strftime('%Y-%m-%d %H:%M:%f') WHERE ROWID = NEW.ROWID AND UpdatedOn = '';
+END;
