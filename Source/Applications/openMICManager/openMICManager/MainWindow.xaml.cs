83e1e1f9ca2632fd817950442546c4c2e3fe1656
StephenCWills <swills@gridprotectionalliance.org>
2017-10-04 15:58:04 -0400

    Fixed build error introduced by breaking change in GSF.

--- Source/Applications/openMICManager/openMICManager/MainWindow.xaml.cs
+++ Source/Applications/openMICManager/openMICManager/MainWindow.xaml.cs
@@ -81,7 +81,6 @@ namespace openMICManager
             Title = ((App)Application.Current).Title;
             TextBoxTitle.Text = AssemblyInfo.EntryAssembly.Title;
 
-            CommonFunctions.CurrentUser = SecurityPrincipal.Identity.Name;
             CommonFunctions.CurrentPrincipal = SecurityPrincipal;
             Title += " - " + SecurityPrincipal.Identity.Provider.UserData.LoginID;
 
