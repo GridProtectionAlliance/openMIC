ab72490e2ee2ace898d3528182d30f19a47c4bce
gsfbuildbot <support@gridprotectionalliance.org>
2017-12-11 00:27:15 -0500

    openMIC: Version change for build v1.2.50.0-master.

--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.49.0")]
-[assembly: AssemblyVersion("1.2.49.0")]
-[assembly: AssemblyFileVersion("1.2.49.0")]
+// [assembly: AssemblyVersion("1.2.50.0")]
+[assembly: AssemblyVersion("1.2.50.0")]
+[assembly: AssemblyFileVersion("1.2.50.0")]
