68570bd22e4e50bdb3eb7cd7d4958666e00ce3be
gsfbuildbot <support@gridprotectionalliance.org>
2019-02-05 00:33:24 -0500

    openMIC: Version change for build v1.3.117.0-master.

--- Source/Tools/BenDownloader/Properties/AssemblyInfo.cs
+++ Source/Tools/BenDownloader/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.116.0")]
-[assembly: AssemblyVersion("1.3.116.0")]
-[assembly: AssemblyFileVersion("1.3.116.0")]
+// [assembly: AssemblyVersion("1.3.117.0")]
+[assembly: AssemblyVersion("1.3.117.0")]
+[assembly: AssemblyFileVersion("1.3.117.0")]
