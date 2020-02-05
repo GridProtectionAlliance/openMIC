b491da14e698572fc71b99a68008638b37d2695b
gsfbuildbot <support@gridprotectionalliance.org>
2020-02-05 13:50:35 -0500

    openMIC: Version change for build v1.3.212.0-master.

--- Source/Applications/openMIC/openMICServices/Properties/AssemblyInfo.cs
+++ Source/Applications/openMIC/openMICServices/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.3.211.0")]
+[assembly: AssemblyVersion("1.3.212.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
