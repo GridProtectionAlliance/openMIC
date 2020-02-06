8169a7b837b3502b200e8b97955fc1f6c79bceb2
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2020-02-06 16:49:16 -0500

    Updated connection profile task to support custom schedule

--- Source/Applications/openMIC/openMIC/Model/ConnectionProfileTask.cs
+++ Source/Applications/openMIC/openMIC/Model/ConnectionProfileTask.cs
@@ -28,10 +28,10 @@ using System.Linq;
 using System.Text;
 using GSF.ComponentModel;
 using GSF.ComponentModel.DataAnnotations;
-using GSF.Configuration;
 using GSF.Data.Model;
 using GSF.TimeSeries.Adapters;
 using Newtonsoft.Json;
+using ConnectionStringParser = GSF.Configuration.ConnectionStringParser<GSF.TimeSeries.Adapters.ConnectionStringParameterAttribute>;
 
 namespace openMIC.Model
 {
@@ -41,15 +41,12 @@ namespace openMIC.Model
         private string m_fileExtensions;
         private string[] m_fileSpecs;
 
-        [ConnectionStringParameter,
-        Description("Defines file names or patterns to download."),
-        DefaultValue("*.*")]
+        [ConnectionStringParameter]
+        [Description("Defines file names or patterns to download.")]
+        [DefaultValue("*.*")]
         public string FileExtensions
         {
-            get
-            {
-                return m_fileExtensions;
-            }
+            get => m_fileExtensions;
             set
             {
                 m_fileExtensions = value;
@@ -57,184 +54,107 @@ namespace openMIC.Model
             }
         }
 
-        public string[] FileSpecs
-        {
-            get
-            {
-                return m_fileSpecs ?? (m_fileSpecs = (m_fileExtensions ?? "*.*").Split(',').Select(pattern => pattern.Trim()).ToArray());
-            }
-        }
+        public string[] FileSpecs => m_fileSpecs ?? (m_fileSpecs = (m_fileExtensions ?? "*.*").Split(',').Select(pattern => pattern.Trim()).ToArray());
 
-        [ConnectionStringParameter,
-        Description("Defines remote path to download files from ."),
-        DefaultValue("/")]
-        public string RemotePath
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines remote path to download files from .")]
+        [DefaultValue("/")]
+        public string RemotePath { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines local path to download files to."),
-        DefaultValue("")]
-        public string LocalPath
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines local path to download files to.")]
+        [DefaultValue("")]
+        public string LocalPath { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if remote folders should scanned for matching downloads - file structure will be replicated locally."),
-        DefaultValue(false)]
-        public bool RecursiveDownload
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if remote folders should scanned for matching downloads - file structure will be replicated locally.")]
+        [DefaultValue(false)]
+        public bool RecursiveDownload { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if remote files should be deleted after download."),
-        DefaultValue(false)]
-        public bool DeleteRemoteFilesAfterDownload
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if remote files should be deleted after download.")]
+        [DefaultValue(false)]
+        public bool DeleteRemoteFilesAfterDownload { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if total remote files to download should be limited by age."),
-        DefaultValue(false)]
-        public bool LimitRemoteFileDownloadByAge
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if total remote files to download should be limited by age.")]
+        [DefaultValue(false)]
+        public bool LimitRemoteFileDownloadByAge { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if old local files should be deleted."),
-        DefaultValue(false)]
-        public bool DeleteOldLocalFiles
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if old local files should be deleted.")]
+        [DefaultValue(false)]
+        public bool DeleteOldLocalFiles { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if download should be skipped if local file already exists and matches remote."),
-        DefaultValue(false)]
-        public bool SkipDownloadIfUnchanged
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if download should be skipped if local file already exists and matches remote.")]
+        [DefaultValue(false)]
+        public bool SkipDownloadIfUnchanged { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if existing local files should be overwritten."),
-        DefaultValue(false)]
-        public bool OverwriteExistingLocalFiles
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if existing local files should be overwritten.")]
+        [DefaultValue(false)]
+        public bool OverwriteExistingLocalFiles { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if existing local files should be archived before new ones are downloaded."),
-        DefaultValue(false)]
-        public bool ArchiveExistingFilesBeforeDownload
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if existing local files should be archived before new ones are downloaded.")]
+        [DefaultValue(false)]
+        public bool ArchiveExistingFilesBeforeDownload { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if downloaded file timestamps should be synchronized to remote file timestamps."),
-        DefaultValue(true)]
-        public bool SynchronizeTimestamps
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if downloaded file timestamps should be synchronized to remote file timestamps.")]
+        [DefaultValue(true)]
+        public bool SynchronizeTimestamps { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines external operation application."),
-        DefaultValue("")]
-        public string ExternalOperation
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines external operation application.")]
+        [DefaultValue("")]
+        public string ExternalOperation { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines maximum amount of time, in seconds, to allow the external operation to sit idle."),
-        DefaultValue(null)]
-        public double? ExternalOperationTimeout
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines maximum amount of time, in seconds, to allow the external operation to sit idle.")]
+        [DefaultValue(null)]
+        public double? ExternalOperationTimeout { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines maximum file size to download."),
-        DefaultValue(1000)]
-        public int MaximumFileSize
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines maximum file size to download.")]
+        [DefaultValue(1000)]
+        public int MaximumFileSize { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines maximum file count to download."),
-        DefaultValue(-1)]
-        public int MaximumFileCount
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines maximum file count to download.")]
+        [DefaultValue(-1)]
+        public int MaximumFileCount { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines directory naming expression."),
-        DefaultValue("<YYYY><MM>\\<DeviceFolderName>")]
-        public string DirectoryNamingExpression
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines directory naming expression.")]
+        [DefaultValue("<YYYY><MM>\\<DeviceFolderName>")]
+        public string DirectoryNamingExpression { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines directory authentication user name."),
-        DefaultValue("")]
-        public string DirectoryAuthUserName
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines directory authentication user name.")]
+        [DefaultValue("")]
+        public string DirectoryAuthUserName { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines directory authentication password."),
-        DefaultValue("")]
-        public string DirectoryAuthPassword
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines directory authentication password.")]
+        [DefaultValue("")]
+        public string DirectoryAuthPassword { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Determines if an e-mail should be sent if the downloaded files have been updated."),
-        DefaultValue(false)]
-        public bool EmailOnFileUpdate
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Determines if an e-mail should be sent if the downloaded files have been updated.")]
+        [DefaultValue(false)]
+        public bool EmailOnFileUpdate { get; set; }
 
-        [ConnectionStringParameter,
-        Description("Defines the recipient e-mail addresses to use when sending e-mails on file updates."),
-        DefaultValue("")]
-        public string EmailRecipients
-        {
-            get;
-            set;
-        }
+        [ConnectionStringParameter]
+        [Description("Defines the recipient e-mail addresses to use when sending e-mails on file updates.")]
+        [DefaultValue("")]
+        public string EmailRecipients { get; set; }
+
+        [ConnectionStringParameter]
+        [Description("Defines the schedule cron override. Blank values assumes use of cron schedule associated with the device.")]
+        [DefaultValue("")]
+        public string Schedule { get; set; }
     }
 
     [PrimaryLabel("Name")]
@@ -244,8 +164,7 @@ namespace openMIC.Model
         #region [ Members ]
 
         // Fields
-        private bool m_success;
-        private StringBuilder m_failMessage;
+        private readonly StringBuilder m_failMessage;
 
         #endregion
 
@@ -262,85 +181,54 @@ namespace openMIC.Model
         #region [ Properties ]
 
         [PrimaryKey(true)]
-        public int ID
-        {
-            get;
-            set;
-        }
+        public int ID { get; set; }
 
-        public int ConnectionProfileID
-        {
-            get;
-            set;
-        }
+        public int ConnectionProfileID { get; set; }
 
         [Required]
         [StringLength(200)]
-        public string Name
-        {
-            get;
-            set;
-        }
+        public string Name { get; set; }
 
         [FieldName("Settings")]
         public string ConnectionString
         {
             get
             {
-                ConnectionStringParser<ConnectionStringParameterAttribute> parser = new ConnectionStringParser<ConnectionStringParameterAttribute>();
+                ConnectionStringParser parser = new ConnectionStringParser();
                 return parser.ComposeConnectionString(Settings);
             }
             set
             {
-                ConnectionStringParser<ConnectionStringParameterAttribute> parser = new ConnectionStringParser<ConnectionStringParameterAttribute>();
+                ConnectionStringParser parser = new ConnectionStringParser();
                 parser.ParseConnectionString(value, Settings);
             }
         }
 
         [NonRecordField]
         [JsonIgnore]
-        public ConnectionProfileTaskSettings Settings
-        {
-            get;
-        }
+        public ConnectionProfileTaskSettings Settings { get; }
 
         [DefaultValueExpression("DateTime.UtcNow")]
-        public DateTime CreatedOn
-        {
-            get;
-            set;
-        }
+        public DateTime CreatedOn { get; set; }
 
         [Required]
         [StringLength(200)]
         [DefaultValueExpression("UserInfo.CurrentUserID")]
-        public string CreatedBy
-        {
-            get;
-            set;
-        }
+        public string CreatedBy { get; set; }
 
         [DefaultValueExpression("this.CreatedOn", EvaluationOrder = 1)]
         [UpdateValueExpression("DateTime.UtcNow")]
-        public DateTime UpdatedOn
-        {
-            get;
-            set;
-        }
+        public DateTime UpdatedOn { get; set; }
 
         [Required]
         [StringLength(200)]
         [DefaultValueExpression("this.CreatedBy", EvaluationOrder = 1)]
         [UpdateValueExpression("UserInfo.CurrentUserID")]
-        public string UpdatedBy
-        {
-            get;
-            set;
-        }
+        public string UpdatedBy { get; set; }
 
         [NonRecordField]
         [JsonIgnore]
-        public bool Success => m_success;
+        public bool Success { get; private set; }
 
         [NonRecordField]
         [JsonIgnore]
@@ -352,13 +240,13 @@ namespace openMIC.Model
 
         public void Reset()
         {
-            m_success = true;
+            Success = true;
             m_failMessage.Clear();
         }
 
         public void Fail(string message = null)
         {
-            m_success = false;
+            Success = false;
             m_failMessage.AppendLine(message);
         }
 
