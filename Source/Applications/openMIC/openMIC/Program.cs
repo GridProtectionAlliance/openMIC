251d86f7c998b4d5a82f5b759fd3be497f44dd9c
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2021-03-20 10:18:29 -0500

    Updated program start operations

--- Source/Applications/openMIC/openMIC/Program.cs
+++ Source/Applications/openMIC/openMIC/Program.cs
@@ -1,14 +1,14 @@
 ﻿//******************************************************************************************************
 //  Program.cs - Gbtc
 //
-//  Copyright © 2015, Grid Protection Alliance.  All Rights Reserved.
+//  Copyright © 2011, Grid Protection Alliance.  All Rights Reserved.
 //
 //  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
 //  the NOTICE file distributed with this work for additional information regarding copyright ownership.
-//  The GPA licenses this file to you under the Eclipse Public License -v 1.0 (the "License"); you may
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may
 //  not use this file except in compliance with the License. You may obtain a copy of the License at:
 //
-//      http://www.opensource.org/licenses/eclipse-1.0.php
+//      http://opensource.org/licenses/MIT
 //
 //  Unless agreed to in writing, the subject software distributed under the License is distributed on an
 //  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
@@ -31,7 +31,7 @@ using GSF.TimeSeries;
 
 namespace openMIC
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         /// The service host instance for the application.
@@ -50,29 +50,25 @@ namespace openMIC
         /// </summary>
         static void Main()
         {
-            if (!s_singleInstanceMutex.WaitOne(0, true))
+            Arguments args = new Arguments(Environment.CommandLine, true);
+
+        #if !DEBUG
+            if (!args.Exists("NoMutex") && !s_singleInstanceMutex.WaitOne(0, true))
                 Environment.Exit(1);
+        #endif
 
             bool runAsService;
             bool runAsApplication;
 
-            Arguments args = new Arguments(Environment.CommandLine, true);
-
-            if (args.Count > 1)
-            {
-                MessageBox.Show("Too many arguments. If specified, argument must be one of: -RunAsService, -RunAsApplication or -RunAsConsole.");
-                Environment.Exit(1);
-            }
-
             if (args.Count == 0)
             {
-#if DEBUG
+            #if DEBUG
                 runAsService = false;
                 runAsApplication = true;
-#else
+            #else
                 runAsService = true;
                 runAsApplication = false;
-#endif
+            #endif
             }
             else
             {
