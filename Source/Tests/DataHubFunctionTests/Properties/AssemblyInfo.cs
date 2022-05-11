dd0507beac5333ef0ee3f6e9e70f5f08b1b3a54f
gsfbuildbot <support@gridprotectionalliance.org>
2022-05-11 00:39:18 -0400

    openMIC: Version change for build v2.0.87.0-master.

--- Source/Tests/DataHubFunctionTests/Properties/AssemblyInfo.cs
+++ Source/Tests/DataHubFunctionTests/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ca5ab7bc-d032-4e9b-ac31-7176ba24febb")]
 
-// [assembly: AssemblyVersion("2.0.86.0")]
-[assembly: AssemblyVersion("2.0.86.0")]
-[assembly: AssemblyFileVersion("2.0.86.0")]
+// [assembly: AssemblyVersion("2.0.87.0")]
+[assembly: AssemblyVersion("2.0.87.0")]
+[assembly: AssemblyFileVersion("2.0.87.0")]
